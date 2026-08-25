1c643b99276afc35177fccbf3e2c659ba5396c3c
Pavle <pavle_nis@yahoo.com>
2026-08-25 10:46:50 +0200

    Rename m_ShowWindow to m_ShowCheatsWindow

--- Mods/Cheats/Src/Cheats.cpp
+++ Mods/Cheats/Src/Cheats.cpp
@@ -101,19 +101,19 @@ void Cheats::OnEngineInitialized() {
 
 void Cheats::OnDrawMenu(zknt::IImGuiRenderer* p_Renderer) {
     if (ImGui::Button(ICON_MD_MAN " CHEATS")) {
-        m_ShowWindow = !m_ShowWindow;
+        m_ShowCheatsWindow = !m_ShowCheatsWindow;
     }
 }
 
 void Cheats::OnDrawUI(zknt::IImGuiRenderer* p_Renderer, bool p_HasFocus) {
-    if (!m_ShowWindow || !p_HasFocus) {
+    if (!m_ShowCheatsWindow || !p_HasFocus) {
         return;
     }
 
     ImGui::SetNextWindowSize({500, 500}, ImGuiCond_FirstUseEver);
 
     ImGui::PushFont(p_Renderer->GetBlackFont());
-    const auto s_IsWindowExpanded = ImGui::Begin(ICON_MD_MAN " Cheats", &m_ShowWindow);
+    const auto s_IsWindowExpanded = ImGui::Begin(ICON_MD_MAN " Cheats", &m_ShowCheatsWindow);
     ImGui::PushFont(p_Renderer->GetRegularFont());
 
     if (s_IsWindowExpanded) {
