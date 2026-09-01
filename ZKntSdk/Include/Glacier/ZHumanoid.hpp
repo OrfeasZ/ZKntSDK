4de3e37e170fecb551d7982d96ccc14000a46225
Pavle <pavle_nis@yahoo.com>
2026-09-01 17:22:05 +0200

    Expand ZHumanoidCharacterEntity with additional fields

--- ZKntSdk/Include/Glacier/ZHumanoid.hpp
+++ ZKntSdk/Include/Glacier/ZHumanoid.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
 #include "ZSpatialEntity.hpp"
+#include "ZValue.hpp"
+
+class ZHumanoidOutfitEntity;
+class IAnimationNetwork;
+class ZAudioEmitterEntity;
 
 class ICharacterEntity : public IComponentInterface {
     virtual ~ICharacterEntity() = 0;
@@ -37,6 +42,24 @@ class ZHumanoidCharacterEntity : public ZSpatialEntity,
     virtual ~ZHumanoidCharacterEntity() = 0;
 
   public:
+    PAD(0x50);                                                     // 0xD8
+    TEntityRef<IAnimationNetwork> m_rAnimationNetwork;             // 0x128
+    TEntityRef<ZAudioEmitterEntity> m_rMouthAudioEmitter;          // 0x140
+    TEntityRef<ZAudioEmitterEntity> m_rLeftFootstepAudioEmitter;   // 0x158
+    TEntityRef<ZAudioEmitterEntity> m_rRightFootstepAudioEmitter;  // 0x170
+    TEntityRef<ZAudioEmitterEntity> m_rCharacterFoleyAudioEmitter; // 0x188
+    TEntityRef<ZAudioEmitterEntity> m_rCloseCombatAudioEmitter;    // 0x1A0
+    TEntityRef<ZSpatialEntity> m_pHeadSpatial;                     // 0x1B8
+    ZResourcePtr m_pFaceFXActor;                                   // 0x1D0
+    ZString m_faceRootBoneName;                                    // 0x1D8
+    bool m_deferredAnimationInitialization;                        // 0x1E8
+    TResourcePtr<ZEntityRef> m_definition;                         // 0x1EC
+    TResourcePtr<ZEntityRef> m_outfitSet;                          // 0x1F4
+    int32 m_selectedOutfit;                                        // 0x1FC
+    uint32_t m_StoreHandle;                                        // 0x200
+    PAD(0xCC);                                                     // 0x204
+    TEntityRef<ZHumanoidOutfitEntity> m_outfit;                    // 0x2D0
+    PAD(0x120);                                                    // 0x2E8
 };
 
 class ZCLTeleportHumanoidEntity : public ZEntityImpl {};
